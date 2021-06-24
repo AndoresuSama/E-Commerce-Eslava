2513e108d64c11a251c0702e95ce0ce72e2c5443
AndoresuSama <aaeg3006@gmail.com>
2021-06-24 19:53:00 -0300

    Se cambia estructura de Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,53 +1,19 @@
 import React from 'react';
 import ItemCount from './ItemCount';
 
-const ItemsArray = [
-  {
-    id: '001',
-    title: 'Taza',
-    price: 'US$15',
-    pictureURL: ''
-  },
-  {
-    id: '002',
-    title: 'Franela',
-    price: 'US$25',
-    pictureURL: ''
-  },
-  {
-    id: '003',
-    title: 'Mochila',
-    price: 'US$30',
-    pictureURL: ''
-  },
-  {
-    id: '004',
-    title: 'Gorra',
-    price: 'US$10',
-    pictureURL: ''
-  }
-];
-
-const Item = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(ItemsArray.map());
-    }, 2000);
-    reject(Error);
-  }
-  );
-};
-
-Item.then(() => {
+const Item = (id, title, price, pictureUrl) => {
   return (
     <div>
-      <ItemCount
-        stock={5}
-        initial={0}
-        onAdd={(x) => alert('Se ha añadido ' + x + ' al carrito')}
+      <p>{id}</p>
+      <h1>{title}</h1>
+      <h2>{price}</h2>
+      <img
+        src={pictureUrl}
+        alt={'Imagen de ' + title}
       />
+      <ItemCount />
     </div>
   );
-});
+};
 
 export default Item;
