c36e29b358dded9eec066db81168242c77c64ff4
AndoresuSama <aaeg3006@gmail.com>
2021-06-24 20:42:59 -0300

    Se agrega gitignore, se cambia estructura de Item.js y ItemList.js. Se crea Item Copy como respaldo del Item.js Anterior

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,46 +1,16 @@
 import React from 'react';
 import ItemCount from './ItemCount';
 
-const ItemsArray = [
-  {
-    id: '001',
-    title: 'Taza',
-    price: 'US$15',
-    pictureURL: ''
-  },
-  {
-    id: '002',
-    title: 'Franela',
-    price: 'US$25',
-    pictureURL: ''
-  },
-  {
-    id: '003',
-    title: 'Mochila',
-    price: 'US$30',
-    pictureURL: ''
-  },
-  {
-    id: '004',
-    title: 'Gorra',
-    price: 'US$10',
-    pictureURL: ''
-  }
-];
-
-const Item = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(ItemsArray.map());
-    }, 2000);
-    reject(Error);
-  }
-  );
-};
-
-Item.then(() => {
+const Item = ({ id, title, price, pictureUrl }) => {
   return (
     <div>
+      <p>{id}</p>
+      <h1>{title}</h1>
+      <h2>{price}</h2>
+      <img
+        src={pictureUrl}
+        alt={'Imagen de ' + title}
+      />
       <ItemCount
         stock={5}
         initial={0}
@@ -48,6 +18,6 @@ Item.then(() => {
       />
     </div>
   );
-});
+};
 
 export default Item;
