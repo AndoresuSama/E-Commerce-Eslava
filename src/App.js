d6b89ea0c5d57c7da4fd2bf2e31a7a1d22b8a152
AndoresuSama <aaeg3006@gmail.com>
2021-07-07 01:18:53 -0300

    Se agrega la libreria react-router-dom y se crea el Wrapper en App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+/* -- Componentes -- */
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
+
+/* -- Estilos -- */
 import './App.css';
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <NavBar />
-      </header>
-      <body className='App-body'>
-        <ItemListContainer
-          saludo='Bienvenido a TheAndoSama Shop'
-          mensaje='La tienda oficial de TheAndoSama'
-        />
-        <ItemDetailContainer />
-      </body>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <header className='App-header'>
+          <NavBar />
+        </header>
+        <body className='App-body'>
+          <Switch>
+            <Route exact path='/'>
+              <ItemListContainer
+                saludo='Bienvenido a TheAndoSama Shop'
+                mensaje='La tienda oficial de TheAndoSama'
+              />
+            </Route>
+            <Route exact path='/Details'>
+              <ItemDetailContainer />
+            </Route>
+          </Switch>
+        </body>
+      </div>
+    </BrowserRouter>
   );
 }
 
