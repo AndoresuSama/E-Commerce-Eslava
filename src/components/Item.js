626234223afa3adfb48352b127f2cac6b925ef98
AndoresuSama <aaeg3006@gmail.com>
2021-07-18 19:08:12 -0300

    Se elimina el ItemCopy ya que este era una prueba con un Item Diferente y no se hacía uso de este componente. Se elimina el ItemCount de Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,17 +8,12 @@ const Item = ({ product }) => {
       <Link to={`/item/${product.id}`}>
         <div id='item'>
           <p>{'#' + product.id}</p>
-          <h1>{product.title}</h1>
-          <h2>{'US$' + product.price}</h2>
           <img
             src={product.pictureURL}
             alt={'Imagen de ' + product.title}
           />
-          <ItemCount
-            stock={product.stock}
-            initial={0}
-            onAdd={(x) => alert('Se ha añadido ' + x + ' al carrito')}
-          />
+          <h1>{product.title}</h1>
+          <h2>{'US$' + product.price}</h2>
         </div>
       </Link>
     </>
